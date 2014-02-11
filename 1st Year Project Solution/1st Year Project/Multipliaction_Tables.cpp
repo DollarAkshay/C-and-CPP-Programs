f4d0c00ea5b23e80dddbeb8ab4bae7fcf73f06b9
DollarAkshay <akshay95aradhya@gmail.com>
2014-02-12 01:11:32 +0530

    Multiplication Table

---
+++ 1st Year Project Solution/1st Year Project/Multipliaction_Tables.cpp
@@ -0,0 +1,10 @@
+/*Shortest Code for Multiplication Tables*/
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int n,i=scanf("%d",&n)-1;
+	while (++i<11)
+		printf("%dx%d=%d\n",n,i,n*i);
+	_getch();
+}
