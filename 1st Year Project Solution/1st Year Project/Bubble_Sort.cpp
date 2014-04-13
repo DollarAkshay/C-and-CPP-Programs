26c3efe749c78505c50b5f54e00aaeda72843f43
DollarAkshay <akshay95aradhya@gmail.com>
2014-04-13 20:50:31 +0530

    Square of Array Elements Added

--- 1st Year Project Solution/1st Year Project/Bubble_Sort.cpp
+++ 1st Year Project Solution/1st Year Project/Bubble_Sort.cpp
@@ -3,22 +3,22 @@
 
 void main(){
 
-	int a[1000], n,i,j;
+	int a[1000], n, i, j;
 
 	printf("Enter the size of the array : ");
-	scanf("%d",&n);
-	printf("\nEnter %d elements : ",n);
-	for (i=0 ; i<n ; i++)
-		scanf("%d",&a[i]);
+	scanf("%d", &n);
+	printf("\nEnter %d elements : ", n);
+	for (i = 0; i<n; i++)
+		scanf("%d", &a[i]);
 
 	//Bubble Sorting
-	for (i = 1; i < n;i++)				//Number of Passes
-		for (j = 0 ; j < n-i ; j++)		//Comparison Loop
-			if (a[j] > a[j + 1]){
-				int temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-			}
+	for (i = 1; i < n; i++)				//Number of Passes
+	for (j = 0; j < n - i; j++)		//Comparison Loop
+	if (a[j] > a[j + 1]){
+		int temp = a[j];
+		a[j] = a[j + 1];
+		a[j + 1] = temp;
+	}
 
 	printf("\nThe sorted array is : ");
 	for (i = 0; i<n; i++)
@@ -26,3 +26,5 @@ void main(){
 
 	_getch();
 }
+
+
---
+++ 1st Year Project Solution/1st Year Project/Square_of_Array_Elements.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+	int i,n, a[1000];
+	printf("Enter array size : ");
+	scanf("%d",&n);
+	printf("Enter the array : ");
+	for (i = 0; i < n; scanf("%d", &a[i++]) );
+	for (i = 0; i < n; printf("%d ", a[i]*a[i++]));
+	_getch();
+}
