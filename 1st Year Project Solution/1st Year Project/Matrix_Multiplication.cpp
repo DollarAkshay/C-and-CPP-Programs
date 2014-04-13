398751929c4ba8b52c1c0bee579d6eba73cff6cf
DollarAkshay <akshay95aradhya@gmail.com>
2014-04-13 21:22:32 +0530

    Matrix Multiplication Correction

--- 1st Year Project Solution/1st Year Project/Matrix_Multiplication.cpp
+++ 1st Year Project Solution/1st Year Project/Matrix_Multiplication.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 
 void main(){
-	int a[10][10], b[10][10], c[10][10]; //               1 2 3 4 5 6 7 8 9
+	int a[10][10], b[10][10], c[10][10]; 
 	int m, n, p, q, i, j, k;
 
 	printf("Enter the size of A : ");
@@ -17,8 +17,10 @@ void main(){
 		for (i = 0; i < p*q; i++)
 			scanf("%d", &b[i/q][i%q]);
 
-		for (i = 0; i < m; i++){			//Speciies Row
-			for (j = 0; j < p; j++){		//Speciies Column
+
+		// Multiplication
+		for (i = 0; i < m; i++){	
+			for (j = 0; j < q; j++){	
 				c[i][j] = k = 0;
 				while (k < n){
 					c[i][j] += a[i][k] * b[k][j];
@@ -28,28 +30,26 @@ void main(){
 			}
 		}
 
+
+		//Printing A,B & C
 		printf("\nMatrix  A : ");
 		for (i = 0; i < m*n; i++){
 			if (i%n == 0)
 				printf("\n");
 			printf("%6d", a[i/n][i%n]);
-	}
-		
+		}	
 		printf("\nMatrix  B : ");
 		for (i = 0; i < p*q; i++){
 			if (i%q == 0)
 				printf("\n");
 			printf("%6d",b[i/q][i%q]);
 		}
-
-
 		printf("\nMatrix  C : ");
 		for (i = 0; i < m*q; i++){
 			if (i%q == 0)
 				printf("\n");
 			printf("%6d", c[i/q][i%q]);
 		}
-
 	}
 	_getch();
 }
