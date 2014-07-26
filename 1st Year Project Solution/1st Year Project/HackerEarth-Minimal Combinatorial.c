b512ef6f18795953a4ebc8ae17d2e728dae7aecd
DollarAkshay <akshay95aradhya@gmail.com>
2014-07-27 01:34:26 +0530

    Smaller code for nCr Golf

--- 1st Year Project Solution/1st Year Project/HackerEarth-Minimal Combinatorial.c
+++ 1st Year Project Solution/1st Year Project/HackerEarth-Minimal Combinatorial.c
@@ -1,7 +1,9 @@
-main(i,t,n,r,m){
-long int x;
-scanf("%d",&t);
-while(x=i=1,scanf("%d%d",&n,&r),r=n-r>r?n-r:r,t--){
-	while(n-r)x=x*n--/i++;
-	printf("%ld\n",x);
-}}
+main(i,n,r,m){
+	long int x;getchar();
+	while(x=i=1,scanf("%d%d",&n,&r)>0){
+		r=n-r>r?n-r:r;
+		while(n-r)x=x*n--/i++;
+		printf("%ld\n",x);
+	}}
+// 126 Characters Code Golf
+
