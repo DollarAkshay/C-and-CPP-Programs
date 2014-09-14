cbdf6628ce64f99fbd5269f86f6d4e9be5e43f25
DollarAkshay <akshay95aradhya@gmail.com>
2014-09-14 20:04:25 +0530

    Added Dynamic Arrays

---
+++ 3rd Semester Programs/Dynamic_Arrays_using_malloc.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void main(){
+
+	int n, i, *A;
+
+	printf("Enter the array size : ");
+	scanf("%d", &n);
+
+	A = (int*)malloc(n*sizeof(int)); //Dynamic Array.Size allocated at runtime
+
+	//Reading array 'A'
+	printf("\nEnter the elements : ");
+	for (i = 0; i < n; i++)
+		scanf("%d", A + i);
+
+	//Printing Array 'A'
+	printf("\nArray A : ");
+	for (i = 0; i < n; i++)
+		printf("%d ", *(A + i));
+	
+	_getch();
+}
+
+/*
+Output :
+---------------------
+Enter the array size : 5
+
+Enter the elements : 1 3 5 2 4
+
+Array A : 1 3 5 2 4
+
+*/
