9284f3578509447fae423ea3193ab707d48e76bf
DollarAkshay <akshay95aradhya@gmail.com>
2014-09-19 00:48:08 +0530

    HackerEarth In love with Primes solved

--- Competitive Coding/HackerEarth_In_love_with_primes.cpp
+++ Competitive Coding/HackerEarth_In_love_with_primes.cpp
@@ -1,3 +1,6 @@
+
+//http://www.hackerearth.com/problem/algorithm/in-love-with-primes/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,24 +17,8 @@ using namespace std;
 #define REP(i,n) FOR(i,0,(int)n-1)
 #define MS0(x) memset(x,0,sizeof(x))
 #define MS1(x) memset(x,1,sizeof(x))
-#define ll _int64
+#define ll long long
 #define MOD 1000000007
-
-int gcd(int a, int b){
-	int c;
-	while (a != 0){
-		c = a;
-		a = b%a;
-		b = c;
-	}
-	return b;
-}
-
-int gcdr(int a, int b){
-	if (a == 0) return b;
-	return gcdr(b%a, a);
-}
-
 
 int isPrime(int x){
 	if (x<2)
@@ -45,25 +32,23 @@ int isPrime(int x){
 	return 1;
 }
 
+
 int main(){
 
-	int t, n, a[10001];
-	a[1] = 0;
-	FOR(i, 2, 100){
-		if (isPrime(i)){
-			a[i] = a[i - 1] + i - 1;
-			continue;
+	int t, n, d;
+	scanf("%d", &t);
+	REP(i, t){
+		d = 0;
+		scanf("%d", &n);
+		FOR(j, 2, n / 2){
+			if (isPrime(j) && isPrime(n - j)){
+				d = 1;
+				break;
+			}
 		}
-		int c = 0;
-		FOR(j, 1, i - 1){
-			if (gcd(j, i) == 1)
-				c++;
-		}
-		a[i] = a[i - 1] + c;
+		puts(d ? "Deepa" : "Arjit");
 	}
-	REP(i, 100){
-		printf("%d, ", a[i]);
-	}
-	
 	return 0;
 }
+
+//Solved
