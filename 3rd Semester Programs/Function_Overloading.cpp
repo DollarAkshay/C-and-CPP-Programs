89620f4a6f6609e85f98448f1a2a38eef8fe1a44
DollarAkshay <akshay95aradhya@gmail.com>
2014-09-21 14:19:53 +0530

    Function Overloading

---
+++ 3rd Semester Programs/Function_Overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int area(int s){
+	return s*s;
+}
+
+int area(float r){
+	return 3.14159*r*r;
+}
+
+int main(){
+
+	int l, h, r;
+	cout << "Enter side : ";
+	cin >> l;
+	cout << "Area of  Square = " << area(l,);
+	cout << "\n\nEnter raduis : ";
+	cin >> r;
+	cout << "Area of Circle = " << area(r);
+	_getch();
+	return 0;
+}
+
+
