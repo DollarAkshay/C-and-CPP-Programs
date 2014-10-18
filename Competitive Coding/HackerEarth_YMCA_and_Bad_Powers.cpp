e54c0fe7801ae4e6d6436a9eebc9aa1b1236e275
DollarAkshay <akshay95aradhya@gmail.com>
2014-10-18 22:57:37 +0530

    Solved a google interview question

--- Competitive Coding/HackerEarth_YMCA_and_Bad_Powers.cpp
+++ Competitive Coding/HackerEarth_YMCA_and_Bad_Powers.cpp
@@ -1,12 +1,13 @@
 
 //http://www.hackerearth.com/codexplod-1/algorithm/ymca-bad-powers/
+//Google Interview Question : http://www.geeksforgeeks.org/find-subarray-with-given-sum/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
-#include<assert.h>
+#include <assert.h>
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -25,58 +26,34 @@ using namespace std;
 #define MOD 1000000007
 #define gc getchar_unlocked
 
-ll int sum[1000000];
-int a[1000000];
+ll int arr[1000005];
 
-void scanint(ll int &x){
-	register int c = gc();
-	x = 0;
-	for (; (c<48 || c>57); c = gc());
-	for (; c>47 && c<58; c = gc()) { x = (x << 1) + (x << 3) + c - 48; }
-}
 
 int main(){
 
-	ll int t, n, p, found, i, j;
+	ll int t, n, p, sum, start, found;
 	scanf("%lld", &t);
 	REP(tc, t){
-		found = 0;
-		MS0(sum);
+
 		scanf("%lld%lld", &n, &p);
 
 		REP(i, n)
-			scanf("%d", &a[i]);
+			scanf("%lld", &arr[i]);
 
-		SORT(a, n);
+		sum = found = start = 0;
 
-		REP(i, n){
-			if (i)
-				sum[i] = sum[i - 1] + a[i];
-			else
-				sum[0] = a[0];
-		}
-
-		for (i = 0; i<n && a[i] <= p; i++){
-			int cunt = 0;
-			for (j = i; j<n; j++){
-				if (sum[j]<p){
-					cunt = 1;
-					break;
-				}
-				if (p == sum[j] - sum[i] + a[i]){
-					found = 1;
-					i = j = n;
-				}
+		FOR(i, 0, n){
+			sum += arr[i];
+			while (sum>p && start<i)
+				sum -= arr[start++];
+			if (sum == p){
+				found = 1;
+				break;
 			}
 		}
-
-		if (!found)
-			puts("No");
-		else
-			puts("Yes");
-
+		puts(found ? "Yes" : "No");
 	}
 	return 0;
 }
 
-//Partially Solved 40/100
+//Solved
