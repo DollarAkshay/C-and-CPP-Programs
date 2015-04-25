0f4e06820cccf01ad96043941e9a87d42b2ca612
DollarAkshay <akshay95aradhya@gmail.com>
2015-04-25 15:38:34 +0530

    Tech Weekend Online Coding Contest

---
+++ Competitive Coding/HackerRank_Tree_City_1.cpp
@@ -0,0 +1,57 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerrank.com/contests/tech-weekend-online-coding/challenges/tree-city-1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+#include <assert.h>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define MOD 1000000007
+#define gc getchar_unlocked
+struct Edge{ int v, w; };
+
+int main(){
+
+	int t;
+	ll int u, v;
+	scanf("%d", &t);
+	REP(tc, t){
+		scanf("%lld%lld", &u, &v);
+		int cost = 0;
+		while (u != v){
+			if (u > v)
+				u = u % 2 ? (u - 1) / 2 : u / 2;
+			else
+				v = v % 2 ? (v - 1) / 2 : v / 2;
+			cost++;
+		}
+		printf("%d\n", cost*2);
+	}
+	return 0;
+}
+
+//Solved
