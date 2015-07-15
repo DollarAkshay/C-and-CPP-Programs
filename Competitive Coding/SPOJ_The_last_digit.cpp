16143f35ef8b6f4fb9722f05e33dd0f679129ff3
DollarAkshay <akshay95aradhya@gmail.com>
2015-07-15 23:08:16 +0530

    SPOJ - :white_check_mark: Weird and silly mistakes :expressionless:

--- Competitive Coding/SPOJ_The_last_digit.cpp
+++ Competitive Coding/SPOJ_The_last_digit.cpp
@@ -9,10 +9,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
+
 using namespace std;
+
 #define FOR(i,a,b) for(int i=a;i<=b;++i)
 #define REP(i,n) FOR(i,0,(int)n-1)
+
 vector<int> cy[10];
+
 void pre(){
 	FOR(i, 0, 9){
 		int v[10];
@@ -32,11 +36,9 @@ int main(){
 	scanf("%d",&t);
 	REP(tc,t){
 		scanf("%d%d",&b,&e);
-		int pat = e%cy[b%10].size()-1;
-		if (pat<0)
-			pat += cy[b%10].size();
-		printf("%d\n",e==0?1:cy[b%10][pat]);
+		printf("%d\n", e==0?1:cy[b%10][(e-1)%cy[b%10].size()]);
 	}
 	return 0;
 }
+
 //Solved
