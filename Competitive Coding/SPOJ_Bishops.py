5fa14aa632d3a7641dda4ff6a9e8897334ba4a80
DollarAkshay <akshay95aradhya@gmail.com>
2015-07-17 16:01:09 +0530

    SPOJ - :chart_with_upwards_trend: 4 Lines in python :joy:

---
+++ Competitive Coding/SPOJ_Bishops.py
@@ -0,0 +1,14 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//http://www.spoj.com/problems/BISHOPS/
+
+import sys
+for line in sys.stdin:
+	x = int(line)
+	print x if x==1 else 2*x-2
+
+//
