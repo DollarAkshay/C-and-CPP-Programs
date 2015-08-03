37a425aad596a9bd31b2455536ea46113c3081da
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-03 22:30:04 +0530

    HackerRank Problems

---
+++ Competitive Coding/HackerRank_Degree_of_Dirtiness.cpp
@@ -0,0 +1,78 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerrank.com/contests/countercode/challenges/degree-of-dirtiness
+
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define pii pair<int,int>
+#define MOD 1000000007
+
+int a[50001];
+
+int main(){
+
+	int t, n, m;
+	scanf("%d", &t);
+	REP(tc, t){
+		scanf("%d%d", &n, &m);
+		MS0(a);
+		int l = 1, r = n;
+		int dir = 1;
+
+		REP(i, m-1){
+			if (dir==1){
+				a[l++]++;
+				dir = -1;
+			}
+			else{
+				a[r--]++;
+				dir = 1;
+			}
+			if (l>r){
+				l = 1;
+				r = n;
+			}
+		}
+		if (dir==1)
+			printf("%d %d\n", l, a[l]);
+		else
+			printf("%d %d\n", r, a[r]);
+	}
+	return 0;
+}
+
+//Solved
