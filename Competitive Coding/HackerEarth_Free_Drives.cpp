21f889d5fa470ef49b88b674ff7cd8e8ced96e81
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-05 23:44:28 +0530

    HackerEarth

--- Competitive Coding/HackerEarth_Free_Drives.cpp
+++ Competitive Coding/HackerEarth_Free_Drives.cpp
@@ -4,19 +4,26 @@
  *                  *
  *~~~~~~~~~~~~~~~~~~*/
 
+//https://www.hackerearth.com/code_mania_40/algorithm/free-drives-1/
 
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include <time.h>
-#include <assert.h>
-#include <algorithm>
-#include <iostream>
+#include <map>
+#include <set>
+#include <deque>
 #include <queue>
 #include <stack>
+#include <bitset>
+#include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
@@ -24,27 +31,27 @@ using namespace std;
 #define FOR(i,a,b) for(int i=a;i<=b;++i)
 #define FORD(i,a,b) for(int i=a;i>=b;--i)
 #define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
 #define MS0(x) memset(x,0,sizeof(x))
 #define MS1(x) memset(x,1,sizeof(x))
 #define SORT(a,n) sort(begin(a),begin(a)+n)
 #define REV(a,n) reverse(begin(a),begin(a)+n)
 #define ll long long
+#define pii pair<int,int>
 #define MOD 1000000007
+
+int a[100000];
 
 int main(){
 
-	srand(time(NULL));
-	FILE *fp = fopen("testcase.txt", "w");
-
-	int n = 100000;
-	fprintf(fp, "%d 2\n",n);
-	REP(i, n)
-		fprintf(fp, "%d ", rand()%21);
-
-	fclose(fp);
-	printf("Done\n");
-	sp;
+	int t, n;
+	scanf("%d", &t);
+	REP(tc, t){
+		scanf("%d", &n);
+		printf("%lld\n", (ll int)n*(n-1)/2);
+	}
 	return 0;
 }
 
-//
+//Solved
