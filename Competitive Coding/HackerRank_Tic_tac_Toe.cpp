15a59017f9977690e80ac4b569a0acf844ff3342
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-29 01:34:25 +0530

    Added TictacToe with Alpha Beta Pruning :D :tree:

--- Competitive Coding/HackerRank_Tic_tac_Toe.cpp
+++ Competitive Coding/HackerRank_Tic_tac_Toe.cpp
@@ -1,23 +1,29 @@
 /*~~~~~~~~~~~~~~~~~~*
- *                  *
- * $Dollar Akshay$  *
- *                  *
- *~~~~~~~~~~~~~~~~~~*/
+*                  *
+* $Dollar Akshay$  *
+*                  *
+*~~~~~~~~~~~~~~~~~~*/
 
 //https://www.hackerrank.com/challenges/tic-tac-toe
 
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include <time.h>
-#include <assert.h>
-#include <algorithm>
-#include <iostream>
+#include <map>
+#include <set>
+#include <deque>
 #include <queue>
 #include <stack>
+#include <bitset>
+#include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
@@ -25,152 +31,167 @@ using namespace std;
 #define FOR(i,a,b) for(int i=a;i<=b;++i)
 #define FORD(i,a,b) for(int i=a;i>=b;--i)
 #define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
 #define MS0(x) memset(x,0,sizeof(x))
 #define MS1(x) memset(x,1,sizeof(x))
 #define SORT(a,n) sort(begin(a),begin(a)+n)
 #define REV(a,n) reverse(begin(a),begin(a)+n)
 #define ll long long
+#define pii pair<int,int>
 #define MOD 1000000007
-#define gc getchar_unlocked
-struct Edge{ int v, w; };
+#define MIN (1<<31)
+#define MAX (~0 - MIN)
 
-char player,opp;
-char Board[3][4];
-int X, Y, turn=1;
+struct point {
+	int x, y;
+};
 
-void transpose(char board[3][4]){
+struct GameState {
+	char board[3][4];
+	vector<point> availableMoves;
+};
 
-	REP(i, 3){
-		FOR(j, 0, i){
-			if (i == j)
-				continue;
-			char t = board[i][j];
-			board[i][j] = board[j][i];
-			board[j][i] = t;
-		}
+char player, opponent;
+int turn;
+GameState original;
+
+bool checkPlayerWin(char board[3][4],char player) {
+
+	REP(i, 3) {
+		if ((board[0][i]==player && board[1][i]==player && board[2][i]==player) ||
+			(board[i][0]==player && board[i][1]==player && board[i][2]==player))
+			return true;
 	}
+
+	if ((board[0][0]==player && board[1][1]==player && board[2][2]==player) ||
+		(board[0][2]==player && board[1][1]==player && board[2][0]==player))
+		return true;
+	else
+		return false;
 }
 
-int checkWinLoss(char board[3][4]){
+int scoreBoard(char board[3][4]) {
 
-	char *ptr;
-	REP(i, 3){
-		ptr = strstr(board[i], "XXX");
-		if (ptr)
-			return player == 'X' ? 1 : -1;
+	if (checkPlayerWin(board, opponent))
+		return -100;
+	else if (checkPlayerWin(board, player))
+		return 100;
+	else
+		return 0;
 
-		ptr = strstr(board[i], "OOO");
-		if (ptr)
-			return player == 'O' ? 1 : -1;
-	}
-	transpose(board);
-	REP(i, 3){
-		ptr = strstr(board[i], "XXX");
-		if (ptr){
-			transpose(board);
-			return player == 'X' ? 1 : -1;
+}
+
+int minimax(GameState node, int alpha, int beta, bool maximizingPlayer) {
+
+
+	int score = scoreBoard(node.board);
+	if (score || node.availableMoves.size()==0)
+		return score + (score<0?-1:1)*node.availableMoves.size();
+
+	if (maximizingPlayer) {
+		int value = MIN;
+		REP(i, node.availableMoves.size()) {
+			point move = node.availableMoves[i];
+			GameState child = node;
+			child.board[move.y][move.x] = player;
+			child.availableMoves.erase(child.availableMoves.begin()+i);
+			value = max(value, minimax(child, alpha, beta, !maximizingPlayer));
+			alpha = max(value, alpha);
+			if (beta<=alpha)
+				break;
 		}
-
-		ptr = strstr(board[i], "OOO");
-		if (ptr){
-			transpose(board);
-			return player == 'O' ? 1 : -1;
+		return value;
+	}
+	else {
+		int value = MAX;
+		REP(i, node.availableMoves.size()) {
+			point move = node.availableMoves[i];
+			GameState child = node;
+			child.board[move.y][move.x] = opponent;
+			child.availableMoves.erase(child.availableMoves.begin()+i);
+			value = min(value, minimax(child, alpha, beta, !maximizingPlayer));
+			beta = min(value, beta);
+			if (beta<=alpha)
+				break;
 		}
+		return value;
 	}
 
-	transpose(board);
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == 'X'){
-		return player == 'X' ? 1 : -1;
-	}
-	else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == 'O'){
-		return player == 'O' ? 1 : -1;
-	}
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == 'X'){
-		return player == 'X' ? 1 : -1;
-	}
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == 'O'){
-		return player == 'O' ? 1 : -1;
-	}
 	return 0;
 }
 
-int calcScore(char board[3][4],int lvl,int mm){
+point getBestMove() {
 
-	int score = 0;
-	if (mm == 0)
-		score = 100;
-	else
-		score = -100;
-	REP(i, 3){
-		REP(j, 3){
-			if (board[i][j] == '_'){
-				board[i][j] = mm ? player : opp;
-				int WL = checkWinLoss(board);
-				if (WL == 1 && mm == 1 && 10 - lvl > score)
-					score = 10 - lvl;
-				else if (WL == -1 && mm == 0 && lvl - 10 < score)
-					score = lvl - 10;
-				else {
-					int temp = calcScore(board,lvl+1, !mm);
-					if (mm == 1 && temp > score){
-						score = temp;
-					}
-					else if (mm == 0 && temp < score){
-						score = temp;
-					}
-				}
-				board[i][j] = '_';
-			}
+	int bestValue = MIN;
+	point bestMove = { -1, -1 };
+	REP(i, original.availableMoves.size()) {
+		point move = original.availableMoves[i];
+		GameState child = original;
+		child.board[move.y][move.x] = player;
+		child.availableMoves.erase(child.availableMoves.begin()+i);
+		int moveValue = minimax(child, MIN, MAX, false);
+		if (moveValue>bestValue) {
+			bestMove = move;
+			bestValue = moveValue;
 		}
 	}
-	if (score == -100 || score == 100)
-		score -= score;
-	return score;
+	return bestMove;
 }
 
-void miniMax(){
-	if (turn == 3 && player == 'X' && Board[0][0] == 'X' && Board[1][1] == 'O'){
-		Y = 1;
-		X = 2;
-		return;
-	}
-	int max = -1000000,WL;
-	REP(i, 3){
-		REP(j, 3){
-			if (Board[i][j] == '_'){
-				Board[i][j] = player=='X' ? 'X' : 'O';
-				WL = checkWinLoss(Board);
-				if ( WL == 1){
-					Y = i;
-					X = j;
-					return;
-				}
-				int calc = calcScore(Board, 1, 0);
-				if (calc > max){
-					max = calc;
-					Y = i;
-					X = j;
-				}
-				Board[i][j] = '_';
-			}
+int main() {
+
+	scanf("%c", &player);
+	opponent = player == 'X'?'O':'X';
+
+	REP(i, 3) {
+		getchar();
+		REP(j, 3) {
+			scanf("%c", &original.board[i][j]);
+			if (original.board[i][j]=='_')
+				original.availableMoves.push_back({j,i});
+			else
+				turn++;
 		}
 	}
-	printf("The score of this move is %d\n", max);
-}
 
-int main(){
-
-	cin >> player;
-	cin >> Board[0] >> Board[1] >> Board[2];
-	REP(i, 3) REP(j, 3) if (Board[i][j] != '_') turn++;
-	if (player == 'X')
-		opp = 'O';
-	else
-		opp = 'X';
-	miniMax();
-	printf("%d %d\n", Y, X);
+	if (turn==0) {
+		printf("1 2");
+	}
+	else{
+		point curMove = getBestMove();
+		printf("%d %d", curMove.y, curMove.x);
+	}
 	sp;
 	return 0;
 }
 
-// 148 wins / 0 Losses / 141 Ties ... Pretty Good
+//Perfect Algo :)
+
+/*
+---------------------------------------------------------------------------
+01 function alphabeta(node, depth, ?, ?, maximizingPlayer)
+02      if depth = 0 or node is a terminal node
+03          return the heuristic value of node
+04      if maximizingPlayer
+05          v := -?
+06          for each child of node
+07              v := max(v, alphabeta(child, depth - 1, ?, ?, FALSE))
+08              ? := max(?, v)
+09              if ? ? ?
+10                  break (* ? cut-off *)
+11          return v
+12      else
+13          v := ?
+14          for each child of node
+15              v := min(v, alphabeta(child, depth - 1, ?, ?, TRUE))
+16              ? := min(?, v)
+17              if ? ? ?
+18                  break (* ? cut-off *)
+19          return v
+-----------------------------------------------------------------------------
+
+(* Initial call *)
+alphabeta(origin, depth, -?, +?, TRUE)
+
+*/
