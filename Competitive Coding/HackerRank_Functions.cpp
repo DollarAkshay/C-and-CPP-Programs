76756cbcbbf84d2f1620904c14f4fecb468db4c1
DollarAkshay <akshay95aradhya@gmail.com>
2015-09-03 20:49:47 +0530

    Yyayayay finished C++ basics on HackerRank :joy::joy::stuck_out_tongue_closed_eyes:  :green_heart:

---
+++ Competitive Coding/HackerRank_Functions.cpp
@@ -0,0 +1,34 @@
+/*~~~~~~~~~~~~~~~~~~*
+*                  *
+* $Dollar Akshay$  *
+*                  *
+*~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerrank.com/challenges/c-tutorial-functions
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d) {
+	if (a>=b && a>=c && a>=d)
+		return a;
+	else if (b>=a && b>=c && b>=d)
+		return b;
+	else if (c>=a && c>=b && c>=d)
+		return c;
+	else
+		return d;
+}
+
+int main() {
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	int ans = max_of_four(a, b, c, d);
+	printf("%d", ans);
+
+	return 0;
+}
+
+//Solved
+
