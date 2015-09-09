797d11242db0ea1fa068895aa261484ff81af8d3
DollarAkshay <akshay95aradhya@gmail.com>
2015-09-10 01:23:21 +0530

    HackerRank - Magic Lines 2015 #1

---
+++ Competitive Coding/HackerRank_Make_It_Anagram.cpp
@@ -0,0 +1,32 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerrank.com/contests/magic-lines-september-2015/challenges/make-it-anagram-mglines
+
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	char s1[10010], s2[10010];
+	cin>>s1>>s2;
+	int a[26] = { 0 };
+	for (int i = 0; i<strlen(s1); i++)
+		a[s1[i]-'a']++;
+	for (int i = 0; i<strlen(s2); i++)
+		a[s2[i]-'a']--;
+	int ans = 0;
+	for (int i = 0; i<26; i++)
+		ans += abs(a[i]);
+	cout<<ans<<endl;
+	return 0;
+}
+
+//Solved
