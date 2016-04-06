26ace5ce97745191a5ec3fe539d626d23cb3f0dc
Akshay L Aradhya <akshay95aradhya@gmail.com>
2016-04-06 23:47:02 +0530

    Added Rotation Functions :)

--- OpenGL/Rubics_Cube_Solver.cpp
+++ OpenGL/Rubics_Cube_Solver.cpp
@@ -56,38 +56,37 @@ struct State {
 
 	/*
 	0 - Front 
-	1 - Top
-	2 - Bottom
-	3 - Left
-	4 - Right
-	5 - Back
+	1 - Back
+	2 - Left
+	3 - Right
+	4 - Top
+	5 - Bottom
 	*/
 
-	char cube[6][3][3];
+	char faces[6][3][3];
 
 	State() {
 		REP(k, 6) {
 			REP(i, 3)
 				REP(j, 3)
-				cube[k][i][j] = k;
+				faces[k][i][j] = k;
 		}
 	}
-
 
 	void printCube(int type=0) {
 
 		if (type==0) {
 			REP(i, 3)
-				printf("      %2d%2d%2d      \n", cube[5][i][0], cube[5][i][1], cube[5][i][2]);
+				printf("      %2d%2d%2d      \n", faces[1][i][0], faces[1][i][1], faces[1][i][2]);
 			REP(i, 3)
-				printf("      %2d%2d%2d      \n", cube[1][i][0], cube[1][i][1], cube[1][i][2]);
+				printf("      %2d%2d%2d      \n", faces[4][i][0], faces[4][i][1], faces[4][i][2]);
 			REP(i, 3)
 				printf("%2d%2d%2d%2d%2d%2d%2d%2d%2d\n", 
-					   cube[3][i][0], cube[3][i][1], cube[3][i][2], 
-					   cube[0][i][0], cube[0][i][1], cube[0][i][2], 
-					   cube[4][i][0], cube[4][i][1], cube[4][i][2]);
+					   faces[2][i][0], faces[2][i][1], faces[2][i][2],
+					   faces[0][i][0], faces[0][i][1], faces[0][i][2], 
+					   faces[3][i][0], faces[3][i][1], faces[3][i][2]);
 			REP(i, 3)
-				printf("      %2d%2d%2d      \n", cube[2][i][0], cube[2][i][1], cube[2][i][2]);
+				printf("      %2d%2d%2d      \n", faces[5][i][0], faces[5][i][1], faces[5][i][2]);
 			printf("\n");
 
 		}
@@ -95,7 +94,7 @@ struct State {
 			REP(i, 3) {
 				REP(k, 6) {
 					REP(j, 3)
-						printf("%2d", cube[k][i][j]);
+						printf("%2d", faces[k][i][j]);
 					printf(" ");
 				}
 				printf("\n");
@@ -103,37 +102,278 @@ struct State {
 		}
 	}
 
-	void front_clock() {
+	void front_anticlock() {
 
 		int t[3];
-		REP(i, 3)
-			t[i] = cube[1][2][i];
 
-		REP(i, 3)
-			cube[1][2][i] = cube[3][i][2];
+		REP(i, 3) {
+			t[i] = faces[4][2][2-i];
+			faces[4][2][2-i] = faces[3][2-i][0];
+			faces[3][2-i][0] = faces[5][0][i];
+			faces[5][0][i] = faces[2][i][2];
+			faces[2][i][2] = t[i];
+		}
 
-		REP(i, 3)
-			cube[3][i][2] = cube[2][0][i];
-
-		REP(i, 3)
-			cube[2][0][i] = cube[4][i][0];
-
-		REP(i, 3)
-			cube[4][i][0] = t[i];
+		REP(i, 2) {
+			t[i] = faces[0][0][2-i];
+			faces[0][0][2-i] = faces[0][2-i][2];
+			faces[0][2-i][2] = faces[0][2][i];
+			faces[0][2][i] = faces[0][i][0];
+			faces[0][i][0] = t[i];
+		}
 
 	}
 
+	void front_clock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][2][i];
+			faces[4][2][i] = faces[2][2-i][2];
+			faces[2][2-i][2] = faces[5][0][2-i];
+			faces[5][0][2-i] = faces[3][i][0];
+			faces[3][i][0] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[0][0][i];
+			faces[0][0][i] = faces[0][2-i][0];
+			faces[0][2-i][0] = faces[0][2][2-i];
+			faces[0][2][2-i] = faces[0][i][2];
+			faces[0][i][2] = t[i];
+		}
+
+	}
+
+	void back_anticlock() {
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][0][i];
+			faces[4][0][i] = faces[2][2-i][0];
+			faces[2][2-i][0] = faces[5][2][2-i];
+			faces[5][2][2-i] = faces[3][i][2];
+			faces[3][i][2] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[1][0][2-i];
+			faces[1][0][2-i] = faces[1][2-i][2];
+			faces[1][2-i][2] = faces[1][2][i];
+			faces[1][2][i] = faces[1][i][0];
+			faces[1][i][0] = t[i];
+		}
+	}
+
+	void back_clock() {
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][0][2-i];
+			faces[4][0][2-i] = faces[3][2-i][2];
+			faces[3][2-i][2] = faces[5][2][i];
+			faces[5][2][i] = faces[2][i][0];
+			faces[2][i][0] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[1][0][i];
+			faces[1][0][i] = faces[1][2-i][0];
+			faces[1][2-i][0] = faces[1][2][2-i];
+			faces[1][2][2-i] = faces[1][i][2];
+			faces[1][i][2] = t[i];
+		}
+	}
+
+	void left_anticlock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][2-i][0];
+			faces[4][2-i][0] = faces[0][2-i][0];
+			faces[0][2-i][0] = faces[5][2-i][0];
+			faces[5][2-i][0] = faces[1][i][2];
+			faces[1][i][2] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[2][0][2-i];
+			faces[2][0][2-i] = faces[2][2-i][2];
+			faces[2][2-i][2] = faces[2][2][i];
+			faces[2][2][i] = faces[2][i][0];
+			faces[2][i][0] = t[i];
+		}
+
+	}
+
+	void left_clock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][i][0];
+			faces[4][i][0] = faces[1][2-i][2];
+			faces[1][2-i][2] = faces[5][i][0];
+			faces[5][i][0] = faces[0][i][0];
+			faces[0][i][0] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[2][0][i];
+			faces[2][0][i] = faces[2][2-i][0];
+			faces[2][2-i][0] = faces[2][2][2-i];
+			faces[2][2][2-i] = faces[2][i][2];
+			faces[2][i][2] = t[i];
+		}
+
+	}
+
+	void right_anticlock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][i][2];
+			faces[4][i][2] = faces[1][2-i][0];
+			faces[1][2-i][0] = faces[5][i][2];
+			faces[5][i][2] = faces[0][i][2];
+			faces[0][i][2] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[3][0][2-i];
+			faces[3][0][2-i] = faces[3][2-i][2];
+			faces[3][2-i][2] = faces[3][2][i];
+			faces[3][2][i] = faces[3][i][0];
+			faces[3][i][0] = t[i];
+		}
+
+	}
+
+	void right_clock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[4][2-i][2];
+			faces[4][2-i][2] = faces[0][2-i][2];
+			faces[0][2-i][2] = faces[5][2-i][2];
+			faces[5][2-i][2] = faces[1][i][0];
+			faces[1][i][0] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[3][0][i];
+			faces[3][0][i] = faces[3][2-i][0];
+			faces[3][2-i][0] = faces[3][2][2-i];
+			faces[3][2][2-i] = faces[3][i][2];
+			faces[3][i][2] = t[i];
+		}
+
+	}
+
+	void top_anticlock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[1][0][i];
+			faces[1][0][i] = faces[3][0][i];
+			faces[3][0][i] = faces[0][0][i];
+			faces[0][0][i] = faces[2][0][i];
+			faces[2][0][i] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[4][0][2-i];
+			faces[4][0][2-i] = faces[4][2-i][2];
+			faces[4][2-i][2] = faces[4][2][i];
+			faces[4][2][i] = faces[4][i][0];
+			faces[4][i][0] = t[i];
+		}
+	}
+
+	void top_clock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[1][0][2-i];
+			faces[1][0][2-i] = faces[2][0][2-i];
+			faces[2][0][2-i] = faces[0][0][2-i];
+			faces[0][0][2-i] = faces[3][0][2-i];
+			faces[3][0][2-i] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[4][0][i];
+			faces[4][0][i] = faces[4][2-i][0];
+			faces[4][2-i][0] = faces[4][2][2-i];
+			faces[4][2][2-i] = faces[4][i][2];
+			faces[4][i][2] = t[i];
+		}
+
+	}
+
+	void bottom_anticlock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[0][2][2-i];
+			faces[0][2][2-i] = faces[3][2][2-i];
+			faces[3][2][2-i] = faces[1][2][2-i];
+			faces[1][2][2-i] = faces[2][2][2-i];
+			faces[2][2][2-i] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[5][0][2-i];
+			faces[5][0][2-i] = faces[5][2-i][2];
+			faces[5][2-i][2] = faces[5][2][i];
+			faces[5][2][i] = faces[5][i][0];
+			faces[5][i][0] = t[i];
+		}
+
+	}
+
+	void bottom_clock() {
+
+		int t[3];
+
+		REP(i, 3) {
+			t[i] = faces[0][2][i];
+			faces[0][2][i] = faces[2][2][i];
+			faces[2][2][i] = faces[1][2][i];
+			faces[1][2][i] = faces[3][2][i];
+			faces[3][2][i] = t[i];
+		}
+
+		REP(i, 2) {
+			t[i] = faces[5][0][i];
+			faces[5][0][i] = faces[5][2-i][0];
+			faces[5][2-i][0] = faces[5][2][2-i];
+			faces[5][2][2-i] = faces[5][i][2];
+			faces[5][i][2] = t[i];
+		}
+
+	}
+
+
 	/*
 	0 - Front
-	1 - Top
-	2 - Bottom
-	3 - Left
-	4 - Right
-	5 - Back
+	1 - Back
+	2 - Left
+	3 - Right
+	4 - Top
+	5 - Bottom
 	*/
 
 };
 
+State cube;
 
 double savedmatrix[16];
 
@@ -145,7 +385,7 @@ float v[8][3] = { {-1,+1,+1}, {+1,+1,+1}, {+1,-1,+1}, {-1,-1,+1},
 				  {-1,+1,-1}, {+1,+1,-1}, {+1,-1,-1}, {-1,-1,-1} };
 
 
-color colorList[6] = { color(1,0,0), color(1,0.9,0), color(0.3,0.8,0), color(0,0.6,1),  color(1,0.4,0), color(1,0,0.8) };
+color colorList[6] = { color(0.3,0.8,0), color(0,0.5,1), color(1,0.8,0), color(0.9,0.9,0.9),  color(1,0.4,0), color(0.9,0,0) };
 
 void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
@@ -181,7 +421,6 @@ void motion(int x, int y) {
 
 }
 
-
 void init() {
 	
 	glClearColor(0, 0, 0, 0);
@@ -191,7 +430,7 @@ void init() {
 
 }
 
-void drawFace(float *a, float *b, float *c, float *d, color col) {
+void drawFace(float *a, float *b, float *c, float *d, int face) {
 
 	glColor3f(1, 1, 1);
 	glBegin(GL_QUADS);
@@ -209,10 +448,10 @@ void drawFace(float *a, float *b, float *c, float *d, color col) {
 		jComp[i] = (b[i]-a[i])/2;
 	}
 
-	glColor3f(col.r, col.g, col.b);
 
 	FOR(i, 0, 2) {
 		FOR(j, 0, 2) {
+			glColor3f(colorList[cube.faces[face][i][j]].r, colorList[cube.faces[face][i][j]].g, colorList[cube.faces[face][i][j]].b);
 			glBegin(GL_QUADS);
 			glVertex3f(a[0] + (0.05 + j*0.65)*jComp[0] - (0.05 + i*0.65)*iComp[0] + constComp[0]*0.01*a[0]/fabs(a[0]),
 					   a[1] + (0.05 + j*0.65)*jComp[1] - (0.05 + i*0.65)*iComp[1] + constComp[1]*0.01*a[1]/fabs(a[1]),
@@ -239,12 +478,13 @@ void drawCube() {
 
 	glColor3f(1, 1, 1);
 
-	drawFace(v[0], v[1], v[2], v[3], colorList[0]);		// Front
-	drawFace(v[4], v[5], v[1], v[0], colorList[1]);		// Top
-	drawFace(v[4], v[5], v[6], v[7], colorList[2]);		// Back
-	drawFace(v[2], v[3], v[7], v[6], colorList[3]);		// Bottom
-	drawFace(v[0], v[4], v[7], v[3], colorList[4]);		// Left
-	drawFace(v[1], v[5], v[6], v[2], colorList[5]);		// Right
+	drawFace(v[0], v[1], v[2], v[3], 0);		// Front
+	drawFace(v[5], v[4], v[7], v[6], 1);		// Back
+	drawFace(v[4], v[0], v[3], v[7], 2);		// Left
+	drawFace(v[1], v[5], v[6], v[2], 3);		// Right
+	drawFace(v[4], v[5], v[1], v[0], 4);		// Top
+	drawFace(v[3], v[2], v[6], v[7], 5);		// Bottom
+	
 	
 
 }
@@ -278,8 +518,63 @@ void display() {
 
 }
 
+void keyboard(unsigned char key, int x, int y) {
 
+	if (key=='Q') {
+		cube.front_anticlock();
+		cube.printCube();
+	}
+	else if (key=='q') {
+		cube.front_clock();
+		cube.printCube();
+	}
 
+	if (key=='W') {
+		cube.back_anticlock();
+		cube.printCube();
+	}
+	else if (key=='w') {
+		cube.back_clock();
+		cube.printCube();
+	}
+
+	if (key=='A') {
+		cube.left_anticlock();
+		cube.printCube();
+	}
+	else if (key=='a') {
+		cube.left_clock();
+		cube.printCube();
+	}
+
+	if (key=='S') {
+		cube.right_anticlock();
+		cube.printCube();
+	}
+	else if (key=='s') {
+		cube.right_clock();
+		cube.printCube();
+	}
+
+	if (key=='Z') {
+		cube.top_anticlock();
+		cube.printCube();
+	}
+	else if (key=='z') {
+		cube.top_clock();
+		cube.printCube();
+	}
+
+	if (key=='X') {
+		cube.bottom_anticlock();
+		cube.printCube();
+	}
+	else if (key=='x') {
+		cube.bottom_clock();
+		cube.printCube();
+	}
+
+}
 
 int main(int argc, char *argv[]) {
 
@@ -290,6 +585,7 @@ int main(int argc, char *argv[]) {
 	glutCreateWindow("Rubics Cube");
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
 	glutMotionFunc(motion);
 	glutReshapeFunc(reshape);
 
