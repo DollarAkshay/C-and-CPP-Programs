a87a5ffc4aa825baa818052505db21ce61dc87d2
Akshay L Aradhya <akshay95aradhya@gmail.com>
2016-05-15 22:29:18 +0530

    Debugging another SO question

--- Miscellaneous/Stackoverflow_Debugging.cpp
+++ Miscellaneous/Stackoverflow_Debugging.cpp
@@ -2,22 +2,35 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
 
-	vector<char> test("HelloWorld"), tmp;
+	for (i = 0; i<fs; i++) { 
+		cout<<f[i];
+	}
+	cout<<"\n Generator :"; 
+	for (i = 0; i<gs; i++) { cout<<g[i]; }
+	//Append 0's 
+	int rs=gs-1; 
+	cout<<"\n Number of 0's to be appended: "<<rs; 
+	for (i=fs;i<fs+rs;i++) { 
+		f[i]=0; 
+	} 
+	int temp[20]; 
+	for (i = 0; i<20; i++) { 
+		temp[i] = f[i]; 
+	}
+	cout<<"\n Message after appending O's :";
+	for (i = 0; i<fs+rs; i++) { 
+		cout<<temp[i]; 
+	}
 
-	// method 1
-	tmp = test;
-	sort(tmp.begin(), tmp.begin() + strlen(tmp.begin()));
-	cout << "result: " << tmp.data() << endl;
+	Division(temp, g, fs, gs); 
+	cout<<"Remainder is "<<temp<<endl; 
+	cout<<"\nThe transmitted message is";
 
-	// method 2
-	tmp = test;
-	sort(tmp.begin(), tmp.end());
-	cout << "result: " << tmp.data() << endl;
-
-	return 0;
 }
