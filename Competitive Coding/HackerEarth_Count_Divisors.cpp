80f954a437d5e8460ce6698f51facd010a9a5b59
Akshay L Aradhya <akshay95aradhya@gmail.com>
2016-07-26 20:10:15 +0530

    Solving some Easy Divisor Porblems

---
+++ Competitive Coding/HackerEarth_Count_Divisors.cpp
@@ -0,0 +1,57 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerearth.com/problem/algorithm/count-divisors/
+
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define pii pair<int,int>
+#define MOD 1000000007
+
+int a[100000];
+
+int main(){
+
+	int l, r, k;
+	scanf("%d %d %d", &l, &r, &k);
+	int a = l%k ? l - l%k + k : l;
+	int b = r - r%k;
+	printf("%d", (b-a)/k + 1);
+
+	return 0;
+}
+
+//Solved
